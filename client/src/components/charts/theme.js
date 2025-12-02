213cf149a549c7b03364869c44d225938813c09b
Angelo Nelson <angelonelson.dev@gmail.com>
2025-12-02 18:31:46 +0530

    just setup commonTheme to be used across all charttypes

---
+++ client/src/components/charts/theme.js
@@ -0,0 +1,53 @@
+export const commonTheme = {
+    background: "transparent",
+    textColor: "#ffffff",
+    fontSize: 12,
+    axis: {
+        domain: {
+            line: {
+                stroke: "#777777",
+                strokeWidth: 1
+            }
+        },
+        legend: {
+            text: {
+                fontSize: 13,
+                fill: "#aaaaaa"
+            }
+        },
+        ticks: {
+            line: {
+                stroke: "#777777",
+                strokeWidth: 1
+            },
+            text: {
+                fontSize: 11,
+                fill: "#dddddd"
+            }
+        }
+    },
+    grid: {
+        line: {
+            stroke: "#444444",
+            strokeWidth: 1
+        }
+    },
+    legends: {
+        text: {
+            fill: "#ffffff"
+        }
+    },
+    tooltip: {
+        container: {
+            background: "#333",
+            color: "#fff",
+            fontSize: 12,
+            borderRadius: "4px",
+            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.3)"
+        }
+    }
+};
+
+export const colors = {
+    scheme: 'nivo', 
+};
