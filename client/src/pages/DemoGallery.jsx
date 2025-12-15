c064ebc835d4749be0eba9c301b46c4e63058fba
AyushCoder9 <ayushsingh.ofc1@gmail.com>
2025-12-15 12:00:00 +0530

    feat: add header h1 title text template

--- client/src/pages/DemoGallery.jsx
+++ client/src/pages/DemoGallery.jsx
@@ -2,3 +2,7 @@ import React from 'react';
 import '../styles/DemoGallery.css';
 import BarChart from '../components/charts/BarChart';
 import LineChart from '../components/charts/LineChart';
+import PieChart from '../components/charts/PieChart';
+import TableChart from '../components/charts/TableChart';
+
+const DemoGallery = () => {
