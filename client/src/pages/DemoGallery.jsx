86917854af7244161861b2a8392fc81adccef5d9
AyushCoder9 <ayushsingh.ofc1@gmail.com>
2025-12-17 12:00:00 +0530

    feat: create main grid layout wrapper

--- client/src/pages/DemoGallery.jsx
+++ client/src/pages/DemoGallery.jsx
@@ -9,3 +9,7 @@ const DemoGallery = () => {
     // Static Dummy Data for the charts
 
     // 1. E-Commerce Sales (Bar Chart)
+    const ecommerceData = [
+        { country: "USA", revenue: 450000 },
+        { country: "China", revenue: 380000 },
+        { country: "UK", revenue: 150000 },
